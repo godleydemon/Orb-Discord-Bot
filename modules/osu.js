2a2f910d2ac0517aefc93d82d8b4b9aa47258238
LittleAsheBear <danielngsw3@gmail.com>
2017-06-23 02:37:11 +0900

    osu.js
    .

--- modules/osu.js
+++ modules/osu.js
@@ -79,29 +79,41 @@ module.exports.osu = async(Discord, message, userinput, self, pinkhex, GenericEr
 				getJSON('https://osu.ppy.sh/api/get_user_recent?k=' + osuapikey + '&u=' + username, function(err, res) {
 					if (err) return console.log(err);
 					if (!res) return;
+					let result = res
 
-					getJSON('https://osu.ppy.sh/api/get_beatmaps?k=' + osuapikey + '&b=' + res[0].beatmap_id, function(err, res) {
+					let totalHits = (result[0].count50 * 50) + ((result[0].count100 + result[0].countkatu) * 100) + ((result[0].count300 + result[0].countgeki) * 300);
+					let hitCount = (result[0].countmiss * 0 / 6) + (result[0].count50 * 1 / 6) + ((result[0].count100 + result[0].countkatu) * 2 / 6) + ((result[0].count300 + result[0].countgeki) * 6 / 6);
+					let accuracy = totalHits / (hitCount * 300);
+					console.log(accuracy)
+
+					getJSON('https://osu.ppy.sh/api/get_scores?k=' + osuapikey + '&b=' + result[0].beatmap_id + '&u=' + result[0].user_id, function(err, res) {
 						if (err) return console.log(err);
 						if (!res) return;
-						console.log(res[0]);
+						let findpp = res
+						console.log(findpp[0]);
+						getJSON('https://osu.ppy.sh/api/get_beatmaps?k=' + osuapikey + '&b=' + result[0].beatmap_id, function(err, res) {
+							if (err) return console.log(err);
+							if (!res) return;
+							console.log(res[0]);
 
-						let embed = new Discord.RichEmbed()
-							.setAuthor("osu! result:", "http://vignette4.wikia.nocookie.net/cytus/images/5/51/Osu_icon.png/revision/latest?cb=20141012114218")
-							.setColor(pinkhex)
-							.setDescription("\n"
-								'***Beatmap:***  ' + res[0].title + '***Version:***  ' + res[0].version +
-								'***Score:***  ' + res[0].score + '***Max Combo:***  ' + res[0].maxcombo + '***Rank:***  ' + res[0].rank +
-								'***Star rating:***  ' + parseFloat(res[0].difficultyrating) + '***OD:***  ' + res[0].diff_overall + '***CS:***  ' + res[0].diff_size +
-								'***AR:***  ' + res[0].diff_approach + '***HP:***  ' + res[0].diff_drain)
-							.setThumbnail('https://b.ppy.sh/thumb/' + res[0].beatmapset_id + 'l.jpg')
-							.setURL('https://osu.ppy.sh/b/' + res[0].beatmap_id + '&m=0');
+							let embed = new Discord.RichEmbed()
+								.setAuthor("osu! result:", "http://vignette4.wikia.nocookie.net/cytus/images/5/51/Osu_icon.png/revision/latest?cb=20141012114218")
+								.setColor(pinkhex)
+								.setDescription("\n" + '|' +
+									'**Beatmap:**  ' + res[0].title + '|' + '**Version:**  ' + res[0].version + '|' +
+									'**Score:**  ' + result[0].score + '|' + '**Max Combo:**  ' + result[0].maxcombo + '|' + '**Rank Achieved:**  ' + result[0].rank + '|' + '**Accuracy:**  ' + parseFloat(accuracy).toFixed(2) + '|' +
+									'**Star rating:**  ' + parseFloat(res[0].difficultyrating).toFixed(2) + '|' + '**OD:**  ' + res[0].diff_overall + '**CS:**  ' + res[0].diff_size +
+									'**AR:**  ' + res[0].diff_approach + '**HP:**  ' + res[0].diff_drain + '|' + '**Pp gained:**  ' + findpp[0].pp + '|')
+								.setThumbnail('https://b.ppy.sh/thumb/' + res[0].beatmapset_id + 'l.jpg')
+								.setURL('https://osu.ppy.sh/b/' + res[0].beatmap_id + '&m=0');
 
-						channel.send({
-								embed
-							})
-							.catch(function() {
-								channel.send(GenericErrorMessage, true)
-							})
+							channel.send({
+									embed
+								})
+								.catch(function() {
+									channel.send(GenericErrorMessage, true)
+								})
+						})
 					})
 				})
 			}
